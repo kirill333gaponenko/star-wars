39bc4d2e2cb89555e249f7cec9b8aaa42a11c8db
kirill333gaponenko <kirill56gaponenko@gmail.com>
2025-11-20 20:08:52 +0200

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,27 @@
+import {useEffect, useState} from "react";
+import {base_url} from "../utils/constants.js";
+
 const AboutMe = () => {
+
+    const [aboutMe, setAboutMe] = useState();
+
+    useEffect(() => {
+
+        fetch(`${base_url}/v1/peoples/1`)
+            .then(res => res.json())
+            .then(data => {
+                let {id,edited,created,homeworld,image,...restOfData} = data,
+                setAboutMe(restOfData)//не знаю почему тут ошибка
+                
+            }
+            )
+            .catch(() => setAboutMe('Error'));
+
+    }, [])
+
     return (
-        <div>
-            About me
-        </div>
+        <p className="far-galaxy">{aboutMe}</p>
+
     )
 }
 
